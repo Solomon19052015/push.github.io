ddb38c6b6494a40290c2a7e361868e994a05e43a
Solomon19052015 <42998762+Solomon19052015@users.noreply.github.com>
2021-03-15 14:07:00 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 
 
 var firebaseConfig = {
-    
-    messagingSenderId: "1013433389089"
-  
+    apiKey: "AIzaSyAV2UJXYsPOOTwsfbmw6g_ozy_rVs8zFfA",
+    authDomain: "plushkin-blr.firebaseapp.com",
+    databaseURL: "https://plushkin-blr.firebaseio.com",
+    projectId: "plushkin-blr",
+    storageBucket: "plushkin-blr.appspot.com",
+    messagingSenderId: "1013433389089",
+    appId: "1:1013433389089:web:2bd2daf0e1143a60df5360"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
 console.log(firebase)
 
 var bt_register = $('#register');
